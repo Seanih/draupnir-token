0b75b229e6058bbc77714c84722025f5f7492437
Sean Anih <sean.anih@gmail.com>
2023-01-18 14:06:22 -0500

    completed NFT smart contract

--- contracts/DraupnirToken.sol
+++ contracts/DraupnirToken.sol
@@ -4,60 +4,71 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract DraupnirToken is
-    ERC721,
-    ERC721Enumerable,
-    ERC721URIStorage,
-    Pausable,
-    Ownable
-{
+contract DraupnirToken is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-    uint256 public mintPrice;
-    uint256 public totalNftsMinted;
-    uint256 public maxSupply;
-    uint256 public maxPerWallet;
-    bool public isPublicMintEnabled;
-    string internal baseTokenUri; //url that points to image(s)
-    address payable public withdrawalWallet;
+    string public baseTokenURI;
+    uint256 public MAX_SUPPLY;
+    uint256 public MAX_PER_WALLET;
+    uint256 public MINT_PRICE;
+    mapping(address => uint256) public nftsInWallet;
+    uint256 public totalMintedNFTs = 0;
 
-    constructor() ERC721("DraupnirToken", "DPT") {
-        mintPrice = 0.02 ether;
-        totalNftsMinted = 0;
-        maxSupply = 1000;
-        maxPerWallet = 3;
+    constructor(
+        uint256 _maxSupply,
+        uint256 _walletMax,
+        uint256 _mintPrice
+    ) ERC721("DraupnirToken", "DPT") {
+        baseTokenURI = "ipfs://QmbQKHDSqjMK6C6Y2EABgHTUuPS7zo8tU5gfmXdiXwAb58";
+        MAX_SUPPLY = _maxSupply;
+        MAX_PER_WALLET = _walletMax;
+        MINT_PRICE = _mintPrice;
     }
 
-    function pause() public onlyOwner {
-        _pause();
+    function safeMint(uint256 _quantity) public payable {
+        require(_quantity > 0, "You have to mint at least 1");
+
+        require(
+            _quantity + nftsInWallet[msg.sender] < MAX_PER_WALLET,
+            "You've already minted the max amount of NFTs"
+        );
+
+        // Shows user what the correct value should be
+        uint256 totalPrice = _quantity * MINT_PRICE;
+        require(
+            msg.value == _quantity * MINT_PRICE,
+            string(
+                abi.encodePacked(
+                    "Invalid value entered. It should be: ",
+                    Strings.toString(totalPrice)
+                )
+            )
+        );
+
+        for (uint256 n = 0; n <= _quantity; n++) {
+            _tokenIdCounter.increment();
+            totalMintedNFTs++;
+            nftsInWallet[msg.sender] += 1;
+            uint256 tokenId = _tokenIdCounter.current();
+
+            _safeMint(msg.sender, tokenId);
+        }
     }
 
-    function unpause() public onlyOwner {
-        _unpause();
-    }
-
-    function safeMint(address to, string memory uri) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-    }
+    // The following functions are overrides required by Solidity.
 
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 tokenId,
         uint256 batchSize
-    ) internal override(ERC721, ERC721Enumerable) whenNotPaused {
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
-
-    // The following functions are overrides required by Solidity.
 
     function _burn(uint256 tokenId)
         internal
@@ -66,13 +77,23 @@ contract DraupnirToken is
         super._burn(tokenId);
     }
 
-    function tokenURI(uint256 tokenId)
+    function tokenURI(uint256 _tokenId)
         public
         view
         override(ERC721, ERC721URIStorage)
         returns (string memory)
     {
-        return super.tokenURI(tokenId);
+        require(_exists(_tokenId), "No token linked to that ID");
+
+        return
+            // concats tokenUri, tokenId & .json file-type into a string that is used by nft marketplaces to retrieve NFT images
+            string(
+                abi.encodePacked(
+                    baseTokenURI,
+                    Strings.toString(_tokenId),
+                    ".json"
+                )
+            );
     }
 
     function supportsInterface(bytes4 interfaceId)
--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,16 @@ class MyDocument extends Document {
 			<Html>
 				<Head>
 					{/* add global fonts here */}
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link
+						rel='preconnect'
+						href='https://fonts.gstatic.com'
+						crossOrigin='true'
+					/>
+					<link
+						href='https://fonts.googleapis.com/css2?family=Bona+Nova:ital,wght@0,400;0,700;1,400&family=Eagle+Lake&family=IM+Fell+French+Canon+SC&display=swap'
+						rel='stylesheet'
+					/>
 				</Head>
 				<body>
 					<Main />
